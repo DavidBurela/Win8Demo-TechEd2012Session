d42362f8a2eb3b3e2f538f296d2ef610b0785946
David Burela <David.Burela@gmail.com>
2012-10-07 21:12:00 +1100

    [41:00] Added Icecreams to View & ViewModel
    Created a ObservableCollection<Icecream> Icecreams; in the ViewModel.
    The VM constructor then populates the Icecreams collection.
    The View is updated to have a ListView on it which Databinds to the
    Icecreams collection.
    The ListView is just doing a .ToSting() on each object at the moment.
    The next step will create a DataTemplate to make each object look nicer.

--- TechEdDemo/ViewModels/MainViewModel.cs
+++ TechEdDemo/ViewModels/MainViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
+using TechEdDemo.Models;
 
 namespace TechEdDemo.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<Icecream> Icecreams { get; set; }
+
         public ICommand IncrementCommand { get; set; }
         public ICommand DoubleCommand { get; set; }
 
@@ -35,10 +39,115 @@ namespace TechEdDemo.ViewModels
         {
             // Properties
             Greeting = "Hello World! v2";
+            CreateData();
 
             // Commands
             IncrementCommand = new DelegateCommand(obj => Increment());
             DoubleCommand = new DelegateCommand(obj => Double());
+        }
+
+        private void CreateData()
+        {
+            Icecreams = new ObservableCollection<Icecream>();
+            Uri _baseUri = new Uri("ms-appx:///");
+
+            Icecream icecream;
+
+            icecream = new Icecream();
+            icecream.Name = "Banana Blast Frozen Yogurt";
+            icecream.SetImage(_baseUri, "Assets/Images/60Banana.png");
+            icecream.Category = "Low-fat frozen yogurt";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Lavish Lemon Ice";
+            icecream.SetImage(_baseUri, "Assets/Images/60Lemon.png");
+            icecream.Category = "Sorbet";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Marvelous Mint";
+            icecream.SetImage(_baseUri, "Assets/Images/60Mint.png");
+            icecream.Category = "Gelato";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Creamy Orange";
+            icecream.SetImage(_baseUri, "Assets/Images/60Orange.png");
+            icecream.Category = "Sorbet";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Succulent Strawberry";
+            icecream.SetImage(_baseUri, "Assets/Images/60Strawberry.png");
+            icecream.Category = "Sorbet";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Very Vanilla";
+            icecream.SetImage(_baseUri, "Assets/Images/60Vanilla.png");
+            icecream.Category = "Ice Cream";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Creamy Caramel Frozen Yogurt";
+            icecream.SetImage(_baseUri, "Assets/Images/60SauceCaramel.png");
+            icecream.Category = "Low-fat frozen yogurt";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Chocolate Lovers Frozen Yougurt";
+            icecream.SetImage(_baseUri, "Assets/Images/60SauceChocolate.png");
+            icecream.Category = "Low-fat frozen yogurt";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Roma Strawberry";
+            icecream.SetImage(_baseUri, "Assets/Images/60Strawberry.png");
+            icecream.Category = "Gelato";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Italian Rainbow";
+            icecream.SetImage(_baseUri, "Assets/Images/60SprinklesRainbow.png");
+            icecream.Category = "Gelato";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Straweberry";
+            icecream.SetImage(_baseUri, "Assets/Images/60Strawberry.png");
+            icecream.Category = "Ice Cream";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Strawberry Frozen Yogurt";
+            icecream.SetImage(_baseUri, "Assets/Images/60Strawberry.png");
+            icecream.Category = "Low-fat frozen yogurt";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Bongo Banana";
+            icecream.SetImage(_baseUri, "Assets/Images/60Banana.png");
+            icecream.Category = "Sorbet";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Firenze Vanilla";
+            icecream.SetImage(_baseUri, "Assets/Images/60Vanilla.png");
+            icecream.Category = "Gelato";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Choco-wocko";
+            icecream.SetImage(_baseUri, "Assets/Images/60SauceChocolate.png");
+            icecream.Category = "Sorbet";
+            Icecreams.Add(icecream);
+
+            icecream = new Icecream();
+            icecream.Name = "Chocolate";
+            icecream.SetImage(_baseUri, "Assets/Images/60SauceChocolate.png");
+            icecream.Category = "Ice Cream";
+            Icecreams.Add(icecream);
         }
 
         public void Increment()
