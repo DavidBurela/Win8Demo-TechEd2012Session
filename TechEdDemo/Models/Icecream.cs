fc1029e5745e6e7ca39ad36d44bac7d3532104d5
David Burela <David.Burela@gmail.com>
2012-10-07 21:07:07 +1100

    [38:00] Created the Icecream model
    Created out new "business model" which is an Icecream

---
+++ TechEdDemo/Models/Icecream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TechEdDemo.Models
+{
+    public class Icecream
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public ImageSource Image { get; set; }
+
+        public void SetImage(Uri baseUri, String path)
+        {
+            Image = new BitmapImage(new Uri(baseUri, path));
+        }
+    }
+}
